78c04e8fab7b2dbc1038015bc62d8024071f7229
Nehal Naeem Haji <nh07884@st.habib.edu.pk>
2025-02-04 15:00:34 +0500

    Add basic file representation

---
+++ q2/solution/mona_lisa.py
@@ -0,0 +1,26 @@
+import random
+import os
+
+# DnaDrawing class
+class DnaDrawing:
+
+    # Attributes
+    width = 0
+    height = 0
+    is_dirty = False
+    genes = []
+
+    def __init__(self):
+        pass
+
+    def mutate():
+        pass
+
+class DnaPolygon:
+
+class DnaPoint:
+
+class DnaBrush:
+
+class NewFitnessCalculator:
+
